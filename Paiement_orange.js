9d5a7e30ab75887ee284d49d0894378e1da439e5
CaroDtoc <olgadonkeng@gmail.com>
2026-04-29 13:23:40 +0200

    docs: ajout d'une nouvelle ligne dans le fichier paiement_orange

--- Paiement_orange.js
+++ Paiement_orange.js
@@ -1,3 +1,4 @@
 function payerORANGE(montant, téléphone) {
     console.log('Paiement ORANGE:'  + montant + 'FCFA vers '+ telephone);
+    console.log('Paiement ORANGE effectué avec succès');git 
 }
